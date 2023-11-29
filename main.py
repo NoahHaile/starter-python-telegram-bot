1cf8cb80e473a31e8cff3d7155fbc12c91369a0f
Noah Hailegiorgis <noahhaile456@gmail.com>
2023-11-28 19:11:19 -0800

    Updating Pydantic base model

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import pg8000
 
 import logging
 import re
+from typing import Optional
+
 
 import time
 import asyncio
@@ -42,7 +44,8 @@ while True:
 
 class TelegramUpdate(BaseModel):
     update_id: int
-    message: dict
+    message: Optional[dict] = None
+    callback_query: Optional[dict] = None
 
 app = FastAPI()
 
